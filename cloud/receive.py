a77e3faee80ee140bc59cfde41e6399beb8d8fb2
Ni Tianzhen <nitianzhen1210@gmail.com>
2020-11-05 23:36:11 +0800

    Save training data to csv

--- cloud/receive.py
+++ cloud/receive.py
@@ -12,7 +12,7 @@ model = None
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Successfully connected to broker.")
-        client.subscribe("cloud/classify/#")
+        client.subscribe([("gateway/sensor_data/#", 0), ("gateway/sensor_data_labelled", 0)])
     else:
         print("Connection failed with code: %d" % rc)
 
@@ -22,19 +22,27 @@ def classify(data):
     return result
 
 def on_message(client, userdata, msg):
-    clientId = msg.topic[msg.topic.rfind('/') + 1:]
-    message = msg.payload.decode()
-    params = message.split(',')
-    light = float(params[0])
-    humidity = float(params[1])
-    humidity_temp = float(params[2])
-    pressure = float(params[3])
-    pressure_temp = float(params[4])
-    data = [light, humidity, humidity_temp, pressure, pressure_temp]
-    result = classify(data)[0]
+    if msg.topic.find("gateway/sensor_data/") != -1:
+        clientId = msg.topic[msg.topic.rfind('/') + 1:]
+        message = msg.payload.decode()
+        print("Predicting:", message)
+        params = message.split(',')
+        light = float(params[0])
+        humidity = float(params[1])
+        humidity_temp = float(params[2])
+        pressure = float(params[3])
+        pressure_temp = float(params[4])
+        data = [light, humidity, humidity_temp, pressure, pressure_temp]
+        result = classify(data)[0]
 
-    print("Sending results:", result)
-    client.publish("cloud/result/" + clientId, str(result))
+        print("Sending results:", result)
+        client.publish("cloud/result/" + clientId, str(result))
+    elif msg.topic.find("gateway/sensor_data_labelled") != -1:
+        message = msg.payload.decode()
+        print("Saving data:", message)
+        csvFile = open("data/data_raw.csv", "a+")
+        csvFile.write(message + "\n")
+        csvFile.close()
 
 def setup(hostname):
     client = mqtt.Client()
